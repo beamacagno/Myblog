fec726c79ea7caa8219a6fcacb16f23af1a1fdbb
beamacagno <bea.macagno@gmail.com>
2021-06-14 12:58:51 +0200

    My Django Girls app, first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\bmacagno\PycharmProjects\myBlog_10062021\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
